ad29e554a2835d4da068933b5ab65e0a5cad05f8
Toni Galata <batscott@gmail.com>
2020-03-25 19:54:15 -0600

    react and rails update

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/shared/Home';
+import About from './components/shared/About';
+import NoMatch from './components/shared/NoMatch';
+import Navbar from './components/shared/Navbar';
+import { Container } from 'semantic-ui-react';
+import Pokedex from './components/pokemon/Pokedex';
+
+
+const App = () => (
+  <>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/pokedex' component={Pokedex} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Container>
+  </>
+)
+export default App;
---
+++ client/src/components/pokemon/Pokedex.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Pokedex extends Component {
+
+  render() {
+    return(
+      <>
+        <h1>Pokedex</h1>
+      </>
+    )
+  }
+}
+
+export default Pokedex;
