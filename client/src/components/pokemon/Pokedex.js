078952d3d9873efd0f634b779cd248f57ad22a00
Toni Galata <batscott@gmail.com>
2020-03-25 21:03:44 -0600

    end of lecture

--- client/src/components/pokemon/Pokedex.js
+++ client/src/components/pokemon/Pokedex.js
@@ -1,14 +1,41 @@
-import React, {Component} from 'react';
-
-class Pokedex extends Component {
-
+import React, { Component } from 'react';
+import axios from 'axios';
+import Pokelist from './Pokelist';
+class Pokedex extends Component { 
+  state = { pokemons: [] }
+  componentDidMount() {
+    axios.get('/api/pokemons')
+      .then( res => {
+        this.setState({ pokemons: res.data })
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
+  updatePokemon = (id, pokemon) => {
+    // { pokemon: { name: 'asdf', type: '',...}}
+    axios.put(`/api/pokemons/${id}`, { pokemon })
+      .then( res => {
+        const pokemons = this.state.pokemons.map( p => {
+          if (p.id === id) {
+            return res.data
+          }
+          return p
+        })
+        this.setState({ pokemons })
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
   render() {
+    const { pokemons } = this.state
     return(
       <>
         <h1>Pokedex</h1>
+        <Pokelist pokemons={pokemons} updatePokemon={this.updatePokemon} />
       </>
     )
   }
 }
-
 export default Pokedex;
